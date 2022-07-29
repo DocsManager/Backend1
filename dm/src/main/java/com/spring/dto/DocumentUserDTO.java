7051c9a4f682d94d5e8ccda024727dfe68fd5e05
Gloryjeong <vles153@naver.com>
2022-07-29 15:44:37 +0900

    mybatis to jpa

--- dm/src/main/java/com/spring/dto/DocumentUserDTO.java
+++ dm/src/main/java/com/spring/dto/DocumentUserDTO.java
@@ -1,14 +1,17 @@
 package com.spring.dto;
 
+
 import com.spring.model.Authority;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DocumentUserDTO {
 
 	private Long userNo;
